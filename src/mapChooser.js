f78ede1985f8ab25fd47108e866d6be11e4a27f9
Jason Asselin <asselin.jason@gmail.com>
2018-12-20 21:02:49 -0500

    Basic TDD cycle

---
+++ src/mapChooser.js
@@ -0,0 +1,8 @@
+function mapChooser(locationName) {
+    if (!locationName)
+        locationName = "default";
+    let imageName = locationName + ".jpg";
+    return imageName;
+}
+
+export default mapChooser;
