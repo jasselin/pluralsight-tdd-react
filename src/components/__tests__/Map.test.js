d2899b192105ac63ad598b5abe0ee8613b48532f
Jason Asselin <asselin.jason@gmail.com>
2018-12-23 13:52:25 -0500

    Basic state implementation

--- src/components/__tests__/Map.test.js
+++ src/components/__tests__/Map.test.js
@@ -4,9 +4,14 @@ import Map from "../Map";
 
 describe("Map", function() {
     let mountedMap;
+    let props;
 
     beforeEach(() => {
-        mountedMap = shallow(<Map />);
+        props = {
+            location: undefined,
+            imageName: "testmap.png"
+        };
+        mountedMap = shallow(<Map imageName={props.imageName} />);
     });
 
     it("renders without crashing", () => {
@@ -19,7 +24,13 @@ describe("Map", function() {
     });
 
     it("displays the none map when no params are given", () => {
-        const defaultMap = mountedMap.find('img[src="images/none.png"]');
-        expect(defaultMap.length).toBe(1);
+        let defaultMap = shallow(<Map />);
+        const defaultImage = defaultMap.find('img[src="images/none.png"]');
+        expect(defaultImage.length).toBe(1);
+    });
+
+    it("displays the map imageName passed to it", () => {
+        const testMap = mountedMap.find("img[src='images/testmap.png']");
+        expect(testMap.length).toBe(1);
     });
 });
--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -4,16 +4,35 @@ import Button from "../components/Button";
 import Map from "../components/Map";
 
 class StoreLocator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentMap: null
+        };
+        this.shops = [
+            {
+                location: "Portland",
+                address: "123 Portland Dr"
+            },
+            {
+                location: "Astoria",
+                address: "123 Astoria Dr"
+            },
+            { location: "", address: "" }
+        ];
+    }
     render() {
+        let storeButtons = this.shops.map((shop, id) => {
+            return <Button key={id} location={shop.location} />;
+        });
         return (
             <div>
                 <Header />
-                <div>
-                    <Button location="Portland" />
-                    <Button location="Astoria" />
-                    <Button />
-                </div>
-                <Map />
+                <div>{storeButtons}</div>
+                <Map
+                    imageName={this.state.currentMap}
+                    location={this.props.location}
+                />
             </div>
         );
     }
