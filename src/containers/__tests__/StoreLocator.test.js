e135060c75eb0d035f894ef9411327bb7787989a
Jason Asselin <asselin.jason@gmail.com>
2018-12-24 15:05:59 -0500

    Axios, mocks

--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -1,11 +1,46 @@
 import React from "react";
 import { shallow } from "enzyme";
 import StoreLocator from "../StoreLocator";
+import axios from "axios";
 
 describe("StoreLocator", function() {
     let mountedStoreLocator;
     beforeEach(() => {
         mountedStoreLocator = shallow(<StoreLocator />);
+    });
+
+    it("calls axios.get in #componentDidMount", () => {
+        return mountedStoreLocator
+            .instance()
+            .componentDidMount()
+            .then(() => {
+                expect(axios.get).toHaveBeenCalled();
+            });
+    });
+
+    it("calls axios.get with correct url", () => {
+        return mountedStoreLocator
+            .instance()
+            .componentDidMount()
+            .then(() => {
+                expect(axios.get).toHaveBeenCalledWith(
+                    "http://localhost:3000/data/shops.json"
+                );
+            });
+    });
+
+    it("updates state with api data", () => {
+        return mountedStoreLocator
+            .instance()
+            .componentDidMount()
+            .then(() => {
+                expect(mountedStoreLocator.state()).toHaveProperty("shops", [
+                    {
+                        location: "test location",
+                        address: "test address"
+                    }
+                ]);
+            });
     });
 
     it("renders without crashing", () => {
@@ -15,11 +50,6 @@ describe("StoreLocator", function() {
     it("renders a header", () => {
         const headers = mountedStoreLocator.find("Header");
         expect(headers.length).toBe(1);
-    });
-
-    it("renders three buttons", () => {
-        const buttons = mountedStoreLocator.find("Button");
-        expect(buttons.length).toBe(3);
     });
 
     it("renders a map", () => {
